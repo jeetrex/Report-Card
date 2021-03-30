4936fe74d172d44982b6d0adc9017455d1acccd3
Abhijit Ganguly <abhijeet288@gmail.com>
2021-03-31 00:33:03 +0530

    V1

---
+++ index.js
@@ -0,0 +1,31 @@
+const excelToJson = require('convert-excel-to-json');
+
+const result = excelToJson({
+	sourceFile: './data/Class LKG - Examination Results.xlsx',
+	header: {
+		rows: 3,
+	}, 
+}); 
+ 
+students = result.Class; 
+
+console.log(students);  
+
+// load the things we need 
+var express = require('express');  
+var app = express();
+app.use(express.static(__dirname + '/public'));
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');  
+
+// use res.render to load up an ejs view file
+ 
+// index page
+app.get('/', function (req, res) {
+	res.render('pages/junior', { students: students });
+});
+
+app.listen(8080); 
+console.log('8080 is the magic port');   
+      
